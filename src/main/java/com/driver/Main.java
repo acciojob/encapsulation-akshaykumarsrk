470a26b2f7df977adff5ee88c4dd74d9652fb47f
acciojob <akshay121817@gmail.com>
2024-10-12 23:05:11 +0530

    Encapsulated data

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String args[])
+    {
+        RWOnly obj = new RWOnly();
+        obj.seta(2);
+        obj.setb(3);
+
+        System.out.println(obj.geta());
+        System.out.println(obj.getb());
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,28 @@
 package com.driver;
 
 public class RWOnly {
+    private int a;
+    private int b;
+
+    public void seta(int a)
+    {
+        this.a = a;
+    }
+
+    public void setb(int b)
+    {
+        this.b = b;
+    }
+
+    public int geta()
+    {
+        return a;
+    }
+
+    public int getb()
+    {
+        return b;
+    }
+    
     
 }
